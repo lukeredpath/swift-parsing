b6b290aa860fbafbb0e61a92acb9743551ad4ef8
Luke Redpath <luke@lukeredpath.co.uk>
2022-09-27 14:16:47 +0100

    Add compiler bug examples

--- Parsing.playground/Contents.swift
+++ Parsing.playground/Contents.swift
@@ -1,89 +1,48 @@
 import Parsing
 
-/*
- This playground builds a parser for [day 13][AoC13] of the Advent of Code 2021 challenge.
+var strings: [String] = ["a", "b", "c"]
 
- [AoC13]: https://adventofcode.com/2021/day/13
- */
-
-/// Sample input to be parsed.
-let input = """
-  6,10
-  0,14
-  9,10
-  0,3
-  10,4
-  4,11
-  6,0
-  6,12
-  4,1
-  0,13
-  10,12
-  3,4
-  3,0
-  8,4
-  1,10
-  2,14
-  8,10
-  9,0
-
-  fold along y=7
-  fold along x=5
-  """
-
-// MARK: - Models
-
-struct Dot {
-  let x, y: Int
+func compactMapStrings1(_ strings: [String]) -> [String] {
+    strings.compactMap {
+        if $0 == "a" { return nil }
+        return $0.uppercased()
+    }
 }
 
-enum Direction: String, CaseIterable {
-  case x, y
+func compactMapStrings2(_ strings: [String]) -> [String] {
+    // This seems to be favoring `Parser.compactMap` even though it returns a completely
+    // different type (`Parsers.CompactMap`) to this function. The compiler error would
+    // not make any sense if it was choosing the correct `Array.compactMap` as that takes
+    // a closure that returns an optional.
+    strings.compactMap {
+        if $0 == "a" { return nil } // error: 'nil' is incompatible with return type 'String'
+        if $0 == "b" {
+            return 123 // no error here as expected
+        }
+        return $0.uppercased()
+    }
 }
 
-struct Fold {
-  let direction: Direction
-  let position: Int
+func compactMapStrings3(_ strings: [String]) -> [String] {
+    strings.compactMap { value -> String? in
+        if value == "a" { return nil }
+        if value == "b" {
+            return "x"
+        }
+        return value.uppercased()
+    }
 }
 
-struct Instructions {
-  let dots: [Dot]
-  let folds: [Fold]
+// The errors on this version make it really clear its choosing the wrong overload.
+func compactMapStrings4(_ strings: [String]) -> [String] {
+    // Cannot convert return expression of type 'Parsers.CompactMap<[String], String>' to return type [String]
+    strings.compactMap { value -> String? in
+        if value == "a" { return nil } // Type '()' cannot conform to 'StringProtocol'
+        if value == "b" {
+            return 123 // no error here as expected
+        }
+        return value.uppercased()
+    }
 }
 
-// MARK: - Parsers
-
-let dot = ParsePrint(.memberwise(Dot.init)) {
-  Digits()
-  ","
-  Digits()
-}
-
-let fold = ParsePrint(.memberwise(Fold.init)) {
-  "fold along "
-  Direction.parser()
-  "="
-  Digits()
-}
-
-let instructions = ParsePrint(.memberwise(Instructions.init)) {
-  Many {
-    dot
-  } separator: {
-    "\n"
-  } terminator: {
-    "\n\n"
-  }
-  Many {
-    fold
-  } separator: {
-    "\n"
-  }
-}
-
-// MARK: Round-trip
-
-let parsed = try instructions.parse(input)
-let printed = try instructions.print(parsed)
-
-input == printed
+print("✅")
